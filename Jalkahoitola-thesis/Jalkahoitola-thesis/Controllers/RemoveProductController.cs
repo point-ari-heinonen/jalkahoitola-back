91a0db5994f8355e74cd3b6dbd38bd9b5a43198e
point-ari-heinonen <ari.heinonen@stud.pointcollege.fi>
2018-01-14 16:08:43 +0200

    removeproductgroup added

--- Jalkahoitola-thesis/Jalkahoitola-thesis/Controllers/RemoveProductController.cs
+++ Jalkahoitola-thesis/Jalkahoitola-thesis/Controllers/RemoveProductController.cs
@@ -35,3 +35,30 @@ namespace Jalkahoitola_thesis.Controllers
         
     }
 }
+
+
+//
+/*
+     public int Get(int? id)
+        {
+
+
+            JalkahoitolaEntities entities = new JalkahoitolaEntities();
+            List<int?> items = (from o in entities.Recieved_ammounts
+                                where o.ProductId == id
+                                select o.UnitStock).ToList();
+            
+            if(items.Sum() == 0 || items.Sum() == null)
+            {
+                Product ProductToBeRemoved = (from o in entities.Products
+                                              where o.ProductId == id
+                                              select o).First();
+               entities.Products.Remove(ProductToBeRemoved);
+                entities.SaveChanges();
+            }
+
+            entities.Dispose();
+            return 1;
+        }
+        
+ */
---
+++ Jalkahoitola-thesis/Jalkahoitola-thesis/Controllers/RemoveProductGroupController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Jalkahoitola_thesis.Controllers
+{
+    public class RemoveProductGroupController : ApiController
+    {
+        // GET: api/RemoveProductGroup
+        
+        //  [HttpGet]
+        public int Get(int id)
+        {
+            JalkahoitolaEntities entities = new JalkahoitolaEntities();
+           
+            int items = (from p in entities.Products
+                         where p.GroupId == id
+                         select p).Count();
+
+            if (items == 0)
+            {
+                ProductGroup ProductGroupToBeRemoved = (from p in entities.ProductGroups
+                                                        where p.GroupId == id
+                                                        select p).First();
+                entities.ProductGroups.Remove(ProductGroupToBeRemoved);
+                entities.SaveChanges();
+            }
+
+            entities.Dispose();
+            return items;
+        }
+
+
+
+    }
+}
